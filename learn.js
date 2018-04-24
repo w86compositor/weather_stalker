2ed185f557909f10b80a10553cdf2aa3485d1880
Waldo Lavaut <w86compositor@gmail.com>
2018-04-24 14:55:50 -0400

    Working on some CSS animations

--- learn.js
+++ learn.js
@@ -1,102 +1,112 @@
 $(document).ready(function () {
+  // hiding all css weather animations
+  $("#the-sun").hide();
+  $("#clouds").hide();
+  $(".card").hide();
+
 
   function findMyLocation() {
     let tempCel = "C";
     let tempFah = "F";
     let result = document.getElementById("out");
 
-    if(!navigator.geolocation) {
+    if (!navigator.geolocation) {
       result.innerHTML = "Geolocation is not supported by your browser";
       return;
     }
-    
+
     function success(position) {
       let latitude = "lat=" + position.coords.latitude;
       let longitude = "lon=" + position.coords.longitude;
-      
+
       let askLoc = `https://fcc-weather-api.glitch.me/api/current?${latitude}&${longitude}`
-      
+
       //Make the AJAX request
-        var xhttp = new XMLHttpRequest();
-       xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-             let resp = JSON.parse(this.responseText);
-             $("#name").html(resp.name  + ", ");
-             $("#country").html(resp.sys.country);
-             $("h2.celcius").html(Math.floor(resp.main.temp) + String.fromCharCode(176) + tempCel)
-             $("#desc").html(resp.weather[0].description);
-            //  $("#icon").append('<img src= "' + resp.weather[0].icon + '">');
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          let resp = JSON.parse(this.responseText);
+          $("#name").html(resp.name + ", ");
+          $("#country").html(resp.sys.country);
+          $("h2.celcius").html(Math.floor(resp.main.temp) + String.fromCharCode(176) + tempCel)
+          $("#desc").html(resp.weather[0].description);
+          //  $("#icon").append('<img src= "' + resp.weather[0].icon + '">');
 
-            // Adding an event listener to toggle between celcius and fahrenheit
-            $("span.slider").on("click", function() {
-              // Calculating celcius and fahrenheit...it's just Math dude
-              let fahrenheit = Math.floor((resp.main.temp * 1.8) + 32);
-              let celcius = Math.floor((fahrenheit - 32) / 1.8);
+          // Adding an event listener to toggle between celcius and fahrenheit
+          $("span.slider").on("click", function () {
+            // Calculating celcius and fahrenheit...it's just Math dude
+            let fahrenheit = Math.floor((resp.main.temp * 1.8) + 32);
+            let celcius = Math.floor((fahrenheit - 32) / 1.8);
 
-              //Checking between celcius and fahrenheit
-              if($("h2.celcius").hasClass("celcius")) {
-               $("h2.celcius").removeClass("celcius").addClass("fahrenheit");
-               $("h2.fahrenheit").html(fahrenheit + String.fromCharCode(176) + tempFah);
-              } else if($("h2.fahrenheit").hasClass("fahrenheit")) {
-                $("h2.fahrenheit").removeClass("fahrenheit").addClass("celcius");
-                $("h2.celcius").html(celcius + String.fromCharCode(176) + tempCel);
-              }
-            });//closing event listener
+            //Checking between celcius and fahrenheit
+            if ($("h2.celcius").hasClass("celcius")) {
+              $("h2.celcius").removeClass("celcius").addClass("fahrenheit");
+              $("h2.fahrenheit").html(fahrenheit + String.fromCharCode(176) + tempFah);
+            } else if ($("h2.fahrenheit").hasClass("fahrenheit")) {
+              $("h2.fahrenheit").removeClass("fahrenheit").addClass("celcius");
+              $("h2.celcius").html(celcius + String.fromCharCode(176) + tempCel);
+            }
+          });//closing event listener
 
-            //check the weather description to display cool css animations
-            let checkWeatherDesc = $("span#desc").html();
-            switch(checkWeatherDesc){
-              case "clear sky":
-              console.log("Shiny day");
+
+
+          //check the weather description to display cool css animations
+          let checkWeatherDesc = $("span#desc").html();
+          switch (checkWeatherDesc) {
+            case "clear sky":
+              // hiding all css weather animations
+              $("#the-sun").show();
               break;
-              
-              case "few clouds":
-              console.log("just a few clouds");
+
+            case "few clouds":
+              // hiding all css weather animations
+              $("#the-sun").show();
+              $("#clouds").show();
               break;
-              
-              case "scattered clouds":
+
+            case "scattered clouds":
               console.log("scatt clouds");
               break;
 
-              case "broken clouds":
+            case "broken clouds":
               console.log("broken clouds");
               break;
 
-              case "shower rain":
+            case "shower rain":
               console.log("shower rain");
               break;
 
-              case "rain":
+            case "rain":
               console.log("it's pouring dude");
               break;
 
-              case "thunderstorm":
+            case "thunderstorm":
               console.log("it's going crazy");
               break;
 
-              case "snow":
+            case "snow":
               console.log("freezing it's snowing");
               break;
 
-              case "mist":
+            case "mist":
               console.log("mist mister");
               break;
-            }
-          }//closing xhttp if statement
-       }; // closing xhttp callback function
-          xhttp.open("GET", `${askLoc}`, true);
-          xhttp.send();
+          }
+        }//closing xhttp if statement
+      }; // closing xhttp callback function
+      xhttp.open("GET", `${askLoc}`, true);
+      xhttp.send();
 
     } //closing bracket of success function
-    
+
     function error() {
       result.innerHTML = "How am I going to give you the weather if you deny the location??";
     }
- 
+
     //calling the navigator object
     navigator.geolocation.getCurrentPosition(success, error);
   } // closing function findMyLocation
-findMyLocation();
+  findMyLocation();
 }); // closing document.ready
 
 
