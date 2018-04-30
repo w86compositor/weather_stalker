724147201818c1efa497e8761d5aa4e679fc02c4
Waldo Lavaut <w86compositor@gmail.com>
2018-04-30 13:46:55 -0400

    Added a disclaimer parapgraph
    Just to mention that sometimes the project does not show the correct location...not sure why.

--- js/learn.js
+++ js/learn.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   // hiding all css weather animations
+  $("p.disclaimer").hide();
   $("#the-sun").hide();
   $("#clouds").hide();
   $("#scattered-cloud").hide();
@@ -60,6 +61,7 @@ $(document).ready(function () {
               console.log(weatherDesc);
               $("#the-sun").show();
               $(".card").show();
+              $("p.disclaimer").show();
               break;
 
             case "clouds":
@@ -68,47 +70,55 @@ $(document).ready(function () {
               $("#the-sun").show();
               $("#clouds").show();
               $(".card").show();
+              $("p.disclaimer").show();
               break;
 
             case "clouds":
               console.log(weatherDesc);
               $(".card").show();
               $("#scattered-cloud").show();
+              $("p.disclaimer").show();
               break;
 
             case "clouds":
               console.log(weatherDesc);
               $(".card").show();
               $("#scattered-cloud").show();
+              $("p.disclaimer").show();
               break;
 
             case "drizzle":
               console.log(weatherDesc);
               $(".card").show();
               $("#rain").show();
+              $("p.disclaimer").show();
               break;
 
             case "rain":
               console.log(weatherDesc);
               $(".card").show();
               $("#rain").show();
+              $("p.disclaimer").show();
               break;
 
             case "thunderstorm":
               console.log(weatherDesc);
               $(".card").show();
               $("#thunderstorm").show();
+              $("p.disclaimer").show();
               break;
 
             case "snow":
               console.log(weatherDesc);
               $(".card").show();
               $("#snow").show();
+              $("p.disclaimer").show();
               break;
 
             case "atmosphere":
               console.log(weatherDesc);
               $(".card").show();
+              $("p.disclaimer").show();
               document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1495733014470-54e8000f94c3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=309d1b63a5335ef23d17ca3572bc2705&auto=format&fit=crop&w=1050&q=80')";
               break;
           }
